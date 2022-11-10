a3e42ade475c7ac372687d068463092ed6258edb
Mervin Fansler <mef3005@med.cornell.edu>
2022-11-10 14:33:05 -0500

    newer gnuconfig

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,9 @@
 
 export DISABLE_AUTOBREW=1
 
+# swap in updated config.sub and config.guess
+rm -f build/autoconf/config.{sub,guess}
+cp $BUILD_PREFIX/share/gnuconfig/config.{sub,guess} build/autoconf/.
+
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
